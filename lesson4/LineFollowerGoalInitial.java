259063bf2efb8a867642adc92067c1ecf8682e12
TobiasEmilHarbo <tobiasharbo@gmail.com>
2016-03-07 12:03:13 +0100

    INDENT

--- lesson4/LineFollowerGoalInitial.java
+++ lesson4/LineFollowerGoalInitial.java
@@ -11,48 +11,44 @@ import lejos.nxt.*;
  * left motor should be connected to port C and the right 
  * motor to port B.
  * 
- * @author  Ole Caprani
+ * @author	Ole Caprani
  * @version 20.02.13
  */
 public class LineFollowerGoalInitial
 {
-  private static int greenMeasurement;
+	public static void main (String[] arg) throws Exception
+	{
+		final int power = 80;
 
-public static void main (String[] aArg)
-  throws Exception
-  {
-     final int power = 80;
-	  
-     ThreeColorSensor sensor = new ThreeColorSensor(SensorPort.S3);
-	 
-     sensor.calibrate();
-	 
-     LCD.clear();
-     LCD.drawString("Light: ", 0, 2); 
-	 
-     while (! Button.ESCAPE.isDown())
-     {
+		ThreeColorSensor sensor = new ThreeColorSensor(SensorPort.S3);
 
-	     LCD.drawInt(sensor.light(),4,10,2);
-	     LCD.refresh();
-	     
-	     if(sensor.green()) {
-	    	 Car.stop();
-	     }
-	     else if ( sensor.black() ){
-	        Car.forward(power, 0);
-	     }
-	     else {
-	        Car.forward(0, power);
-	     }
-	    
-	     
-	     Thread.sleep(10);
-     }
-     
-     Car.stop();
-     LCD.clear();
-     LCD.drawString("Program stopped", 0, 0);
-     LCD.refresh();
-   }
+		sensor.calibrate();
+
+		LCD.clear();
+		LCD.drawString("Light: ", 0, 2); 
+
+		while (! Button.ESCAPE.isDown())
+		{
+
+			LCD.drawInt(sensor.light(),4,10,2);
+			LCD.refresh();
+
+			if(sensor.green()) {
+				Car.stop();
+			}
+			else if ( sensor.black() ){
+				Car.forward(power, 0);
+			}
+			else {
+				Car.forward(0, power);
+			}
+
+			Thread.sleep(10);
+		}
+
+		Car.stop();
+		LCD.clear();
+		LCD.drawString("Program stopped", 0, 0);
+		LCD.refresh();
+	}
 }
