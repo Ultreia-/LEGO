23c6d954aebb2261098ec5369d080078c2583261
Rasmus Kjeldgaard <Rasmus Kjeldgaard>
2016-04-07 13:58:04 +0200

    BTCON

--- lesson5/GyroTest.java
+++ lesson5/GyroTest.java
@@ -1,5 +1,45 @@
 package lesson5;
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.MotorPort;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.GyroSensor;
 
-public class GyroTest {
-
+/**
+ * Test of Gyro Sensor
+ * Records the minimum, maximum and current values
+ * 
+ * @author Lawrie Griffiths
+ */
+public class GyroTest 
+{
+	public static void main(String[] args) throws Exception
+	{
+		GyroSensor gyro = new GyroSensor(SensorPort.S2);
+		float minValue = 1023, maxValue = 0;
+		int sampleInterval = 5; // ms
+		
+		LCD.drawString("Gyro Test:", 0, 0);
+		Button.waitForAnyPress();
+		
+		LCD.drawString("Min:", 0, 2);
+		LCD.drawString("Max:", 0, 3);
+		LCD.drawString("Current:", 0, 4);
+		
+		while(!Button.ESCAPE.isDown()) 
+		{
+			float value = gyro.readValue();
+			minValue = Math.min(minValue, value);
+			maxValue = Math.max(maxValue, value);
+			
+			
+			
+			LCD.drawInt((int) minValue, 6, 5, 2);
+			LCD.drawInt((int) maxValue, 6, 5, 3);
+			LCD.drawInt((int)(value), 6, 9, 4);
+			
+			Thread.sleep(sampleInterval);
+			
+		}
+	}
 }
