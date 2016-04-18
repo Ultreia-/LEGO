c5be7177fab40998204508989061ca8c7f8bd8e5
TobiasEmilHarbo <tobiasharbo@gmail.com>
2016-04-18 11:39:08 +0200

    init

--- lesson6/Vehicle1.java
+++ lesson6/Vehicle1.java
@@ -1,83 +1,81 @@
-package lesson6;
-
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
-import lesson3.Car;
 
-public class Vehicle1 {
-	
+public class Vehicle1
+{
 	private static SensorPort soundSensor = SensorPort.S1;
 	
-	private static THRESHOLD = 900;
-	
+	private static int THRESHOLD = 500;
+    private int COUNT = 0;
+
 	private static int map(int x, int in_min, int in_max, int out_min, int out_max) {
 		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 	}
-	
+
+    public Vehicle1()
+    {
+        mainLoop();
+    }
+
+    private void mainLoop() {
+        //Is This correct?!
+        soundSensor.setTypeAndMode(SensorPort.TYPE_SOUND_DB, SensorPort.MODE_RAW);
+
+        int soundLevel;
+        int speed1;
+        int speed2;
+
+        while (!Button.ESCAPE.isDown()) {
+            //DIrection = left - right
+            soundLevel = 1023 - soundSensor.readRawValue();
+
+            speed1 = map(soundLevel, 0, 1023, 60, 100);
+            speed2 = map(soundLevel, 0, 1023, 0, -100);
+
+            LCD.drawString("Raw: " + soundLevel, 0, 0);
+            LCD.drawString("Speed: " + speed1, 0, 1);
+
+            checkThresholdAndDance(soundLevel, speed1, speed1);
+        }
+
+        Car.stop();
+        LCD.clear();
+        LCD.drawString("Program stopped", 0, 0);
+
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            //ignore
+        }
+    }
+
 	public static void main(String [] args) throws Exception
     {
-        //Is This correct?!
-		soundSensor.setTypeAndMode(SensorPort.TYPE_SOUND_DB, SensorPort.MODE_RAW);
+        Vehicle1 v = new Vehicle1();
+    }
 
-		int soundLevel;
-		int speed1;
-		int speed2;
+	private void checkThresholdAndDance(int soundlevel, int speed1, int speed2) {
 
-		
-        while (! Button.ESCAPE.isDown())
+		if(soundlevel > THRESHOLD)
         {
-        	//DIrection = left - right
-    		soundLevel = soundSensor.readRawValue();
-
-    		speed1 = map(soundLevel, 0, 1023, 0, 100);	
-    		speed2 = map(soundLevel, 0, 1023, 0, -100);	
-    		
-    		
-    		
-    		LCD.drawString("Raw: " + soundLevel,0,0);
-    		LCD.drawString("Speed: " + speed1,0,1);
-    		    		
-
-			checkThresholdAndDance(soundLevel);
-
-
-
-    		
-       }
-       Car.stop();
-       LCD.clear();
-       LCD.drawString("Program stopped", 0, 0);
-       Thread.sleep(2000); 
-   }
-
-
-	private static checkThresholdAndDance(int soundlevel, int speed1, int speed2) {
-
-
-		if(soundLevel > THRESHOLD){
-
 			dance(speed1);
-
-		} else {
-
-			Car.forward(speed1, speed2);
-
 		}
-
-
+        else
+        {
+			Car.forward(speed1, speed2);
+		}
 	}
 
-	private static dance(int speed) {
-
-
+	private void dance(int speed)
+    {
 		if(COUNT < 4) {
 
 			Car.turnRight(speed);
 
 		} else if (COUNT < 8) {
 
-			Car.turnLEFT(speed);
+			Car.turnLeft(speed);
 
 		}
 
@@ -88,7 +86,11 @@ public class Vehicle1 {
 			COUNT = 0;
 		}
 
-
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+            //ignore
+        }
 	}
 
 }
