01ce8d01064b5f4d5b06539291ab0e0631904379
TobiasEmilHarbo <tobiasharbo@gmail.com>
2016-04-19 09:43:46 +0200

    test car.forward(-100, -100)

---
+++ lesson6/testVehicle.java
@@ -0,0 +1,13 @@
+import lejos.nxt.Button;
+
+public class testVehicle
+{
+	public static void main(String [] args) throws Exception
+    {
+    	while (!Button.ESCAPE.isDown())
+        {
+        	Car.forward(100, -100);
+        	Thread.sleep(100);
+    	}
+    }
+}
